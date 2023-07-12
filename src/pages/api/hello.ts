ff37298f41ddf0cc4aaf07c9a145a543dbecd1f7
cassandraGoose <cassandra.torske@gmail.com>
2023-07-12 09:26:36 -0600

    make page generation wait longer before giving up in next config

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { prisma } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import prisma from '@/lib/prisma';
+
 import { GetStaticProps } from 'next/types';
 
 export const getStaticProps: GetStaticProps = async () => {

d1a45070359effa791342189c2de2fd37d19366e
cassandraGoose <cassandra.torske@gmail.com>
2023-07-12 16:03:29 -0600

    add navbar

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ const { combinedEnv } = loadEnvConfig(process.cwd());
 
 export default defineConfig({
   env: combinedEnv,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here

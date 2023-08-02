9cb50a7de618fcb55a04aa2c9e5b801967ee4bc7
cassandraGoose <cassandra.torske@gmail.com>
2023-08-02 12:17:59 -0600

    setup initial dashboard; housekeeping items

--- cypress/e2e/dashboard_test.cy.ts
+++ cypress/e2e/dashboard_test.cy.ts
@@ -1,5 +1,16 @@
 describe('Dashboard', () => {
   it('can navigate to the dashboard', () => {
     expect(true).to.equal(true);
-  })
+  });
+
+  it('can navigate to the dashboard', () => {
+    
+  });
 });
+
+
+//cy.get('[data-cy="submit"]').click()	
+
+// todo move interfaces into another file
+// todo should the prisma query live here or somewhere else. 
+// todo i have a lot of divs. necessary? 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,33 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      colors: {
+        'blue': '#4FAEAF',
+        'blue-light': '#7CCFCF',
+        'purple': '#607CBD',
+        'purple-light': '#8AA1D7',
+        'warning': '#FFD073',
+        'warning-light': '#FFDD98',
+        'error': '#FF7373',
+        'error-light': '#FF9898',
+        'gray': '#262626',
+        'gray-mid': '#757474',
+        'gray-light': '#d9d9d9',
+        'white': '#f5f5f5'
+      },
+    },
   },
   plugins: [],
 }
+

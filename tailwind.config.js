89290626d2c6ba2e15a10f80340409ddd57798ce
cassandraGoose <cassandra.torske@gmail.com>
2023-08-15 19:43:21 -0600

    remove old color system from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,20 +19,6 @@ module.exports = {
       width: {
         "10xl": "50rem",
       }
-      // colors: {
-      //   'blue': '#4FAEAF',
-      //   'blue-light': '#7CCFCF',
-      //   'purple': '#607CBD',
-      //   'purple-light': '#8AA1D7',
-      //   'warning': '#FFD073',
-      //   'warning-light': '#FFDD98',
-      //   'error': '#FF7373',
-      //   'error-light': '#FF9898',
-      //   'gray': '#262626',
-      //   'gray-mid': '#757474',
-      //   'gray-light': '#d9d9d9',
-      //   'white': '#f5f5f5'
-      // },
     },
   },
   plugins: [require('daisyui')],

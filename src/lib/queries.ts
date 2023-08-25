91294e59a67a6e713206ade479ff9df12e028fe2
cassandraGoose <cassandra.torske@gmail.com>
2023-08-25 16:36:28 -0600

    remove extra await

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,7 +1,7 @@
 import prisma from '@/lib/prisma';
 
-export async function getPathwaysByEmail() {
-  return await prisma.person.findFirst({
+export function getPathwaysByEmail() {
+  return prisma.person.findFirst({
     where: {
       email: process.env.TEST_USER_EMAIL,
     },
@@ -23,10 +23,10 @@ export async function getPathwaysByEmail() {
   });
 }
 
-export async function getPathwayByUserId(userId:string, pathwayId:string) {
+export function getPathwayByUserId(userId:string, pathwayId:string) {
   console.log('prod debug pathway: ', pathwayId);
   console.log('prod debug user:', userId);
-  return await prisma.person.findFirst({
+  return prisma.person.findFirst({
     where: {
       id: userId,
     },

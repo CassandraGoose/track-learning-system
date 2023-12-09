3d8ef7d99040dd62911f884362bb74804ae51eef
cassandraGoose <cassandra.torske@gmail.com>
2023-12-08 18:06:30 -0700

    homeroll collapse to allow for expected user experience

--- src/components/PathwayProgressDetails.tsx
+++ src/components/PathwayProgressDetails.tsx
@@ -1,60 +1,84 @@
-import { ContentArea, Competency } from '../lib/interface';
+import { useState } from "react";
+import { ContentArea, Competency } from "../lib/interface";
 
 export default function PathwayProgressDetails({
   contentAreas,
 }: {
   contentAreas: [ContentArea];
 }) {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <>
+    <div className="flex justify-center items-start mt-0">
       {contentAreas.map((contentArea: ContentArea) => (
-        <div
-          tabIndex={0}
-          key={contentArea.id}
-          className='collapse collapse-plus border border-base-300 bg-base-200 mt-16 rounded-md'>
-          <div className='collapse-title'>
-            <p className='font-medium text-lg' data-cy="content-area-title">{contentArea.title}</p>
-            <p data-cy="content-area-description">{contentArea.description}</p>
-            <div className='flex items-center justify-center pt-8'>
-              <p className='font-thin' data-cy="competency-progress">3/7 competencies met</p>
+        <>
+          <div
+            key={contentArea.id}
+            className="w-full border border-base-300 bg-base-200 mt-16 rounded-md p-4"
+          >
+            <div>
+              <div className="flex justify-between">
+                <p className="font-medium text-lg" data-cy="content-area-title">
+                  {contentArea.title}
+                </p>
+                <button
+                  className="text-4xl"
+                  aria-controls={`${contentArea.title}-details`}
+                  aria-expanded={!collapsed}
+                  onClick={() => setCollapsed(!collapsed)}
+                >
+                  {collapsed ? "+" : "-"}
+                </button>
+              </div>
+              <p data-cy="content-area-description">
+                {contentArea.description}
+              </p>
+              <div className="flex items-center justify-center pt-8">
+                <p className="font-thin" data-cy="competency-progress">
+                  3/7 competencies met
+                </p>
+              </div>
+            </div>
+            <div
+              className={` ${collapsed ? "block" : "hidden"}`}
+              id={`${contentArea.title}-details`}
+            >
+              <table className="table table-zebra table-lg">
+                <thead>
+                  <tr>
+                    <th>Completed</th>
+                    <th>Competency</th>
+                    <th>Description</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {contentArea.competencies.map((competency: Competency) => (
+                    <tr key={competency.id}>
+                      <td>
+                        <span className="flex items-center space-x-3">
+                          <span>✅</span>
+                        </span>
+                      </td>
+                      <td>
+                        <span className="flex items-center space-x-3">
+                          <p>{competency.title}</p>
+                        </span>
+                      </td>
+                      <td>
+                        <p>{competency.description}</p>
+                      </td>
+                      <td>
+                        <button className="btn btn-secondary">Proof</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
-          <div className='collapse-content'>
-            <table className='table table-zebra table-lg'>
-              <thead>
-                <tr>
-                  <th>Completed</th>
-                  <th>Competency</th>
-                  <th>Description</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {contentArea.competencies.map((competency: Competency) => (
-                  <tr key={competency.id}>
-                    <td>
-                      <span className='flex items-center space-x-3'>
-                        <span>✅</span>
-                      </span>
-                    </td>
-                    <td>
-                      <span className='flex items-center space-x-3'>
-                        <p>{competency.title}</p>
-                      </span>
-                    </td>
-                    <td>
-                      <p>{competency.description}</p>
-                    </td>
-                    <td>
-                      <button className='btn btn-secondary'>Proof</button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        </>
       ))}
-    </>
+    </div>
   );
 }
--- src/pages/dashboard/[pathway].tsx
+++ src/pages/dashboard/[pathway].tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { getPathwayByUserId } from '../../lib/queries';
-import PathwayProgressDetails from '../../components/PathwayProgressDetails';
+import React from "react";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { getPathwayByUserId } from "../../lib/queries";
+import PathwayProgressDetails from "../../components/PathwayProgressDetails";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { pathway } = context.params! as { pathway: string };
   // temporarily set up just to grab user 1's version of this pathway. when we have auth, we'll change this.
   try {
     const userPathway = await getPathwayByUserId(
-      'cljvusdou00003ntltwo9mhm5',
+      "cljvusdou00003ntltwo9mhm5",
       pathway
     );
 
@@ -22,7 +22,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     // this try catch in for now, but i'll make a ticket to revisit this after gathering some intel.
     // https://github.com/vercel/next.js/discussions/11180#discussioncomment-2399
     console.error(e);
-    throw new Error('Failed to fetch');
+    throw new Error("Failed to fetch");
   }
 };
 
@@ -31,27 +31,22 @@ export default function Pathway({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const selectedPathway = pathway.pathways[0];
   return (
-    <section className='flex flex-col my-12 items-center mx-12'>
-      <div className='flex w-full p-8 justify-between border rounded-md border-black'>
-        <div className='flex flex-col space-y-8'>
-          <div className='flex flex-col'>
-            <h1 className='text-2xl' data-cy='pathway-title'>
-              {selectedPathway.title}
-            </h1>
-            <div className='flex items-center'>
-              <progress
-                className='progress progress-secondary'
-                value={70}
-                max='100'></progress>
-              <span className='text-secondary-low'>70%</span>
-            </div>
-            <h2 data-cy='pathway-description'>{selectedPathway.description}</h2>
-            <div>
-              <PathwayProgressDetails
-                contentAreas={selectedPathway.contentArea}
-              />
-            </div>
+    <section className="flex flex-col my-12 items-center mx-12">
+      <div className="flex w-full p-8 justify-between border rounded-md border-black">
+        <div className="flex flex-col space-y-8 w-full">
+          <h1 className="text-2xl" data-cy="pathway-title">
+            {selectedPathway.title}
+          </h1>
+          <div className="flex items-center">
+            <progress
+              className="progress progress-secondary"
+              value={70}
+              max="100"
+            ></progress>
+            <span className="text-secondary-low">70%</span>
           </div>
+          <h2 data-cy="pathway-description">{selectedPathway.description}</h2>
+          <PathwayProgressDetails contentAreas={selectedPathway.contentArea} />
         </div>
       </div>
     </section>

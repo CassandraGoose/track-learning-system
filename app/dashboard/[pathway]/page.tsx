b7e08310563b6adb2ef6d540ab06b6d56411d66d
cassandraGoose <cassandra.torske@gmail.com>
2024-01-18 19:22:49 -0700

    remove comments

--- app/dashboard/[pathway]/page.tsx
+++ app/dashboard/[pathway]/page.tsx
@@ -38,13 +38,3 @@ export default async function Pathway({ params }: { params: { pathway: string }}
     </section>
   );
 }
-
-// contentArea: [
-//   {
-//     id: 2,
-//     createdAt: 2023-08-02T22:36:14.906Z,
-//     title: 'General Education',
-//     description: 'Skills and content knowledge',
-//     competencies: [Array]
-//   }
-// ]

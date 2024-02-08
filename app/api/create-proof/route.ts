1dbfc5fc7708d5fc0780bca259fdef73bdb96712
cassandraGoose <cassandra.torske@gmail.com>
2024-02-08 12:00:23 -0700

    remove unnecessary additions after refactor to server actions from api system

--- app/api/create-proof/route.ts
+++
@@ -1,8 +0,0 @@
-import { createProof } from '../../lib/queries';
-
-export async function POST(req: Request) {
-  const data = await req.json();
-  const addProof = await createProof(data);
-
-  return Response.json(addProof);
-}

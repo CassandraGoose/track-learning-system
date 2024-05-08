a44b73eb12a369c02a819c0c17ef7fb5cbcd1ba3
cassandraGoose <cassandra.torske@gmail.com>
2024-05-08 15:31:07 -0600

    add proofs tests

--- tests/pathway_progress_details.spec.ts
+++ tests/pathway_progress_details.spec.ts
@@ -29,6 +29,8 @@ test('updates competency progress when proof is added and removed', async ({ pag
   await page.getByTestId('proof-description-textarea').press('Tab');
   await page.getByTestId('proof-justification-textarea').fill('test');
   await page.getByTestId('new-proof-submit').click();
+    // I know, I know...but because we have to use a DB and can't mock anything reasonably, I have to wait for the db deletion to actually complete before asserting that anything has updated.
+    await page.waitForTimeout(1000); 
   await page.getByTestId('navbar-dashboard-link').click();
   await page.locator('article').filter({ hasText: 'Use TrackLearn to use the' }).getByTestId('view-pathway').click();
   await expect(page.getByTestId('competency-progress')).toContainText('1 / 13 competencies met');
--- tests/proofs.spec.ts
+++ tests/proofs.spec.ts
@@ -1,52 +1,36 @@
-// import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-// test.beforeEach(async ({ page }) => {
-//   await page.goto('http://localhost:3000/dashboard/1/1');
-// });
+test.beforeEach(async ({ page }) => {
+  page.context().clearCookies();
+  await page.goto('http://localhost:3000/login');
+  await page.getByTestId('username').click();
+  await page.getByTestId('username').fill('IAmCass');
+  await page.getByTestId('password').click();
+  await page.getByTestId('password').fill(process.env.TEST_USER_PW || '');
+  await page.getByTestId('login').click();
+});
 
-// test('has a title', async ({ page }) => {
-//   await expect(page.getByTestId('competency-title')).toHaveText('Competency: Find personal pathways');
-// });
 
-// test('has a form', async ({ page }) => {
-//   await expect(page.getByTestId('new-proof-form-title')).toHaveText('Add New Proof');
-//   await expect(page.getByTestId('new-proof-form')).toHaveText('Proof TitleProof DescriptionProofAdd');
-//   await expect(page.getByTestId('proof-title-input')).toBeVisible();
-//   await expect(page.getByTestId('proof-description-textarea')).toBeVisible();
-//   await expect(page.getByTestId('proof-justification-textarea')).toBeVisible();
-// });
+test('add proof', async ({ page }) => {
+  await page.locator('article').filter({ hasText: 'Use TrackLearn to use the' }).getByTestId('view-pathway').click();
+  await page.getByTestId('toggle-competency-details').first().click();
+  await page.getByRole('row', { name: 'View Pathways Explain how to' }).getByTestId('view-proofs').click();
+  await page.getByTestId('proof-title-input').click();
+  await page.getByTestId('proof-title-input').fill('test');
+  await page.getByTestId('proof-title-input').press('Tab');
+  await page.getByTestId('proof-description-textarea').fill('test');
+  await page.getByTestId('proof-description-textarea').press('Tab');
+  await page.getByTestId('proof-justification-textarea').fill('test');
+  await page.getByTestId('new-proof-submit').click();
+  await expect(page.getByTestId('proof-title')).toContainText('test');
+  await expect(page.getByTestId('proof-description')).toContainText('test');
+  await expect(page.getByTestId('proof-justification')).toContainText('test');
+});
 
-// test('submits a proof', async ({ page }) => {
-//   await page.getByTestId('proof-title-input').fill('My Proof');
-//   await page.getByTestId('proof-description-textarea').fill('My description');
-//   await page.getByTestId('proof-justification-textarea').fill('My justification');
-//   await page.getByTestId('new-proof-submit').click();
-//   await expect(page.getByTestId('proof-title').last()).toHaveText('My Proof');
-//   await expect(page.getByTestId('proof-description').last()).toHaveText('My description');
-//   await expect(page.getByTestId('proof-justification').last()).toHaveText('My justification');
-//   //delete to keep tests stand-alone
-//   await page.getByTestId('delete-proof').waitFor({state: 'attached'});
-//   await page.getByTestId('delete-proof').click();
-//   // will use in next pr
-//   // await page.getByTestId('proof-table').waitFor({state: 'detached'});
-// });
-
-// test('removes proof from list', async ({ page }) => {
-//   // add proof to keep tests stand-alone
-
-//   // temp removal for next pr
-//   // await page.getByTestId('proof-title-input').fill('My Proof');
-//   // await page.getByTestId('proof-description-textarea').fill('My description');
-//   // await page.getByTestId('proof-justification-textarea').fill('My justification');
-//   // await page.getByTestId('new-proof-submit').click();
-//   // await expect(page.getByTestId('proof-title').last()).toHaveText('My Proof');
-//   // await expect(page.getByTestId('proof-description').last()).toHaveText('My description');
-//   // await expect(page.getByTestId('proof-justification').last()).toHaveText('My justification');
-
-//   // // actual test
-//   // await expect(page.getByTestId('delete-proof')).toBeVisible();
-//   // await page.getByTestId('delete-proof').click();
-//   // await page.waitForTimeout(5000)
-//   // will use in next pr
-//   // await expect(page.getByTestId('proof-table')).toBeHidden();
-// });
+test('delete proof', async ({ page }) => {
+  await page.locator('article').filter({ hasText: 'Use TrackLearn to use the' }).getByTestId('view-pathway').click();
+  await page.getByTestId('toggle-competency-details').first().click();
+  await page.getByRole('row', { name: 'View Pathways Explain how to' }).getByTestId('view-proofs').click();
+  await expect(page.getByTestId('delete-proof')).toBeVisible();
+  await page.getByTestId('delete-proof').click();
+});
